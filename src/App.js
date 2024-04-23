80cfd0718fda8efa2fa89877ea987d76748ece35
Glenben <gn782000@gmail.com>
2024-04-24 00:25:17 +0300

    ensured the  systm to know which router can be accessed by the different users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Home from "./components/Home";
 import Sidebar from "./components/sideBar";
 import SalesPage from "./components/sales";
 import { AuthProvider, useAuth } from "./components/AuthContext";
-import LoginForm from "./components/loginForm";
+import LoginForm from "./components/loginAdmin";
 import RegistrationForm from "./components/registerForm";
 import ReportsData from "./components/reportForm";
+import UserForm from "./components/userRegForm";
+import LoginUserForm from "./components/loginUser";
 
 function App() {
   return (
@@ -15,46 +18,79 @@ function App() {
     </AuthProvider>
   );
 }
-
 function AppContent() {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, userRole } = useAuth(); // Update useAuth to include userRole
 
   return (
-    <div className="flex flex-col">
-      <BrowserRouter>
-        <div className="flex bg-blue-gray-900">
-          <Sidebar />
-          <div className="ml-0 lg:ml-60 w-screen">
-            <Navbar />
-            <Switch>
-              <Route exact path="/loginForm">
-                <LoginForm />
-              </Route>
-              <Route path="/registerForm">
-                <RegistrationForm/>
-              </Route>
-              {isLoggedIn ? (
-                <>
-                  <Route exact path="/dashboard">
-                    <Home />
-                  </Route>
-                  <Route path="/sales">
-                    <SalesPage />
-                  </Route>
-                  <Route path="/reportForm">
-                      <ReportsData/>
-                    </Route>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/loginAdmin">
+          <LoginForm />
+        </Route>
+        <Route path="/registerForm">
+          <RegistrationForm />
+        </Route>
+        <Route path="/loginUser">
+          <LoginUserForm/>
+        </Route>
+        <Route path="/userRegForm">
+          <UserForm/>
+        </Route>
+        <Route path="/">
+          {isLoggedIn ? (
+            userRole === "admin" ? (
+              <AdminDashboard />
+            ) : (
+              <UserDashboard />
+            )
+          ) : (
+            <Redirect to="/loginAdmin" />
+          )}
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
-                </>
-              ) : (
-                <Route path="/">
-                  <LoginForm />
-                  </Route>
-              )}
-            </Switch>
-          </div>
-        </div>
-      </BrowserRouter>
+function AdminDashboard() {
+  return (
+    <div>
+      <Sidebar />
+      <div className="flex-grow ml-0 lg:ml-60 bg-blue-gray-900">
+        <Navbar />
+        <Switch>
+          <Route exact path="/dashboard">
+            <Home/>
+          </Route>
+          <Route path="/sales">
+            <SalesPage />
+          </Route>
+          <Route path="/reportForm">
+            <ReportsData />
+          </Route>
+          <Redirect to="/dashboard" />
+        </Switch>
+      </div>
+    </div>
+  );
+}
+
+function UserDashboard() {
+  return (
+    <div>
+      <Sidebar />
+      <div className="flex-grow ml-0 lg:ml-60 bg-blue-gray-900">
+        <Navbar />
+        <Switch>
+          <Route exact path="/dashboard">
+            <Home/>
+          </Route>
+          <Route path="/sales">
+            <SalesPage />
+          </Route>
+          {/* <Redirect to="/sales" /> */}
+        </Switch>
+      </div>
     </div>
   );
 }
@@ -62,61 +98,142 @@ function AppContent() {
 export default App;
 
 
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import React from "react";
+// import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 // import Navbar from "./components/NavBar";
 // import Home from "./components/Home";
 // import Sidebar from "./components/sideBar";
 // import SalesPage from "./components/sales";
 // import { AuthProvider, useAuth } from "./components/AuthContext";
-// import LoginForm from "./components/loginForm";
+// import LoginForm from "./components/loginAdmin";
+// import RegistrationForm from "./components/registerForm";
+// import ReportsData from "./components/reportForm";
+// import UserForm from "./components/userRegForm";
+// import LoginUserForm from "./components/loginUser";
 
 // function App() {
 //   return (
 //     <AuthProvider>
-//       <div className="flex flex-col">
-//         <BrowserRouter>
-//           <div className="flex">
-//             <Sidebar />
-//             <div className="ml-0 lg:ml-60 w-screen">
-//               <Navbar />
-//               <Switch>
-//                 <Route exact path="/loginForm">
-//                   <LoginForm />
-//                 </Route>
-//                 <ProtectedRoutes />
-//               </Switch>
-//             </div>
-//           </div>
-//         </BrowserRouter>
-//       </div>
+//       <AppContent />
 //     </AuthProvider>
 //   );
 // }
 
-// // Create a separate component for protected routes
-// function ProtectedRoutes() {
+// function AppContent() {
 //   const { isLoggedIn } = useAuth();
 
 //   return (
-//     <>
-//       {isLoggedIn ? (
-//         <>
+//     <BrowserRouter>
+//       <Switch>
+//         <Route exact path="/loginAdmin">
+//           <LoginForm />
+//         </Route>
+//         <Route path="/registerForm">
+//           <RegistrationForm />
+//         </Route>
+//         <Route path="/loginUser">
+//           <LoginUserForm/>
+//         </Route>
+//         <Route path="/userRegForm">
+//           <UserForm/>
+//         </Route>
+//         <Route path="/">
+//           {isLoggedIn ? (
+//             <Dashboard />
+//           ) : (
+//             <Redirect to="/loginAdmin" />
+//           )}
+//         </Route>
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// }
+
+// function Dashboard() {
+//   return (
+//     <div className="flex flex-col ">
+//       <Sidebar />
+//       <div className="flex-grow ml-0 lg:ml-60 bg-blue-gray-900">
+//         <Navbar />
+//         <Switch>
 //           <Route exact path="/dashboard">
-//             <Home />
+//             <Home/>
 //           </Route>
 //           <Route path="/sales">
 //             <SalesPage />
 //           </Route>
-//         </>
-//       ) : (
-//         <Route path="/loginForm">
-//           <LoginForm />
-//         </Route>
-//       )}
-//     </>
+//           <Route path="/reportForm">
+//             <ReportsData />
+//           </Route>
+//           <Redirect to="/dashboard" />
+//         </Switch>
+//       </div>
+//     </div>
 //   );
 // }
 
 // export default App;
+
+// // import Navbar from "./components/NavBar";
+// // import Home from "./components/Home";
+// // import Sidebar from "./components/sideBar";
+// // import SalesPage from "./components/sales";
+// // import { AuthProvider, useAuth } from "./components/AuthContext";
+// // import LoginForm from "./components/loginAdmin";
+// // import RegistrationForm from "./components/registerForm";
+// // import ReportsData from "./components/reportForm";
+
+// // function App() {
+// //   return (
+// //     <AuthProvider>
+// //       <AppContent />
+// //     </AuthProvider>
+// //   );
+// // }
+
+// // function AppContent() {
+// //   const { isLoggedIn } = useAuth();
+
+// //   return (
+// //     <div className="flex flex-col">
+// //       <BrowserRouter>
+// //         <div className="flex bg-blue-gray-900">
+// //           <Sidebar />
+// //           <div className="ml-0 lg:ml-60 w-screen">
+// //             <Navbar />
+// //             <Switch>
+// //               <Route exact path="/loginAdmin">
+// //                 <LoginForm />
+// //               </Route>
+// //               <Route path="/registerForm">
+// //                 <RegistrationForm/>
+// //               </Route>
+// //               {isLoggedIn ? (
+// //                 <>
+// //                   <Route exact path="/dashboard">
+// //                     <Home />
+// //                   </Route>
+// //                   <Route path="/sales">
+// //                     <SalesPage />
+// //                   </Route>
+// //                   <Route path="/reportForm">
+// //                       <ReportsData/>
+// //                     </Route>
+
+// //                 </>
+// //               ) : (
+// //                 <Route path="/">
+// //                   <LoginForm />
+// //                   </Route>
+// //               )}
+// //             </Switch>
+// //           </div>
+// //         </div>
+// //       </BrowserRouter>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
 
 
