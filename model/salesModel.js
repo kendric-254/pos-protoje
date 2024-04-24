acf3ebae81d844d21bf8ae9549b3c6601e5fe7b0
Glenben <gn782000@gmail.com>
2024-04-24 21:31:24 +0300

    model table for sales

---
+++ model/salesModel.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+    const Sale = sequelize.define('salestb', {
+        sale_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        quantity_sold: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        total_price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    })
+
+    return Sale
+}
