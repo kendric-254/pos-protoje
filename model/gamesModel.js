db99934aa4da71ad3f70ffc457aaac29683cda1a
Glenben <gn782000@gmail.com>
2024-04-24 21:31:00 +0300

    products model table

---
+++ model/gamesModel.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    const Game = sequelize.define('games', {
+        game_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        game_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        quantity_in_stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    })
+
+    return Game
+}
